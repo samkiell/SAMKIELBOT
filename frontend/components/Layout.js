524d02cf7d9e5c1c889ec3da2b4534441ff5aadb
SAMKIEL <samuelezekiel488@gmail.com>
2025-10-28 01:56:34 +0100

    update Layout.js

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-100">
       <Navbar />
-      <div className="pt-20">{children}</div>
+      <div className="pt-21">{children}</div>
     </div>
   );
 }

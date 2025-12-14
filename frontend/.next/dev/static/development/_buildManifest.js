68177767b5390acaaf5d5a7bbe4d51f3d74a8821
SAMKIEL <samuelezekiel488@gmail.com>
2025-12-14 06:43:55 +0100

    refactor: change Hero component from named to default export

--- frontend/.next/dev/static/development/_buildManifest.js
+++ frontend/.next/dev/static/development/_buildManifest.js
@@ -1,13 +1,4 @@
 self.__BUILD_MANIFEST = {
-  "/dashboard": [
-    "static/chunks/pages/dashboard.js"
-  ],
-  "/deploy": [
-    "static/chunks/pages/deploy.js"
-  ],
-  "/login": [
-    "static/chunks/pages/login.js"
-  ],
   "__rewrites": {
     "afterFiles": [],
     "beforeFiles": [],

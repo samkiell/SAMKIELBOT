f45d6837605edb3bcac7d44f238273cc7bdb07b1
SAMKIEL <samuelezekiel488@gmail.com>
2026-02-07 22:35:56 +0100

    feat: initialize SAMKIEL Bot Next.js application with global layout, context providers, metadata, and essential services.

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
 import "react-quill/dist/quill.snow.css";
 
-const AuthProvider = dynamic(
-  () => import("../lib/auth").then((mod) => mod.AuthProvider),
-  {
-    ssr: false,
-  },
-);
+import { AuthProvider } from "../lib/auth";
 
 export default function App({ Component, pageProps }) {
   return (
